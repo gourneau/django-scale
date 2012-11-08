f4c8df9fa6002ad3ed726690b94d7d6e70174dbc
Joshua Gourneau <josh@gourneau.com>
2012-11-08 12:44:23 -0800

    no cache

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -3,8 +3,11 @@ from django.shortcuts import get_object_or_404, render, render_to_response
 from django.http import HttpResponse
 import models
 from django.views.decorators.cache import cache_page
+from django.views.decorators.cache import never_cache
 
-@cache_page(4)
+#@cache_page(4)
+
+@never_cache
 def home(request):
     a = models.Post.objects.all().order_by('?')[:1].get()
     return render_to_response('index.html', {'a': a})
