8f035c00cd9023299a264f8ecbb3d40fb12328b0
root <root@games.(none)>
2012-11-08 20:15:00 +0000

    first

---
+++ myproject/myapp/views.py
@@ -0,0 +1,15 @@
+from django.views.generic.base import TemplateView
+from django.shortcuts import get_object_or_404, render, render_to_response
+from django.http import HttpResponse
+import models
+from django.views.decorators.cache import cache_page
+
+@cache_page(4)
+def home(request):
+    a = models.Post.objects.all().order_by('?')[:1].get()
+    return render_to_response('index.html', {'a': a})
+
+def blitz(request):
+    return HttpResponse("42")
+
+
